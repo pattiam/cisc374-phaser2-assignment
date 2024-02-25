93abda11c15fbee906fee321a2c0980f89943acf
patricktiamson <pattiam@udel.edu>
2024-02-25 13:33:58 -0500

    phaser part 2 browser test

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -2,6 +2,8 @@ import Phaser from "phaser";
 
 export default class MainScene extends Phaser.Scene {
     private platforms?: Phaser.Physics.Arcade.StaticGroup;
+    private player?: Phaser.Physics.Arcade.Sprite;
+    private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
 
     constructor() {
         super({ key: "MainScene" });
@@ -9,7 +11,6 @@ export default class MainScene extends Phaser.Scene {
 
     create() {
         this.add.image(400, 300, "sky");
-
         this.platforms = this.physics.add.staticGroup();
         const ground = this.platforms.create(
             400,
@@ -22,6 +23,35 @@ export default class MainScene extends Phaser.Scene {
         this.platforms.create(50, 250, "ground");
         this.platforms.create(750, 250, "ground");
 
+        this.player = this.physics.add.sprite(100, 450, "dude");
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: "left",
+            frames: this.anims.generateFrameNames("dude", { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: "turn",
+            frames: [{ key: "dude", frame: 4 }],
+            frameRate: 20,
+        });
+
+        this.anims.create({
+            key: "right",
+            frames: this.anims.generateFrameNumbers("dude", {
+                start: 5,
+                end: 8,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        this.physics.add.collider(this.player, this.platforms);
+
         const message = `Phaser v${Phaser.VERSION}`;
         this.add
             .text(this.cameras.main.width - 15, 15, message, {
@@ -32,6 +62,22 @@ export default class MainScene extends Phaser.Scene {
     }
 
     update() {
-        // this.fpsText.update();
+        if (!this.cursors) {
+            return;
+        }
+        if (this.cursors.left?.isDown) {
+            this.player?.setVelocityX(-160);
+            this.player?.anims.play("left", true);
+        } else if (this.cursors.right.isDown) {
+            this.player?.setVelocityX(160);
+            this.player?.anims.play("right", true);
+        } else {
+            this.player?.setVelocity(0);
+            this.player?.anims.play("turn");
+        }
+
+        if (this.cursors.up?.isDown && this.player?.body?.touching.down) {
+            this.player.setVelocity(-330);
+        }
     }
 }
